57388f297c0813764990381624fc8187d7e114cf
BomberQLF <tmurphyparis@gmail.com>
2024-12-07 11:16:11 +0100

    Update

--- day1/src/App.jsx
+++ day1/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Password from './Components/Password';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Password />
+      <Footer />
     </div>
   )
 }
