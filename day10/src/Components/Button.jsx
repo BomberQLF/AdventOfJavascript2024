8f757fef0bfa936baa0388be25d10e5d9c68a395
BomberQLF <tmurphyparis@gmail.com>
2024-12-14 18:23:25 +0100

    Framer motion animation

--- day10/src/Components/Button.jsx
+++ day10/src/Components/Button.jsx
@@ -1,27 +1,47 @@
-import React, { useState } from 'react';
-import Close from '../assets/close.svg';
-import Megaphone from '../assets/megaphone.svg';
-import Links from './Links';
+import React, { useState } from "react";
+import Close from "../assets/close.svg";
+import Megaphone from "../assets/megaphone.svg";
+import Links from "./Links";
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Button = () => {
-    const [buttonClicked, setButtonClicked] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
 
-
-    return (
-        <div className="button-container">
-            <button className='button' onClick={() => setButtonClicked(!buttonClicked)}>
-                {buttonClicked ? (
-                    <img className='closeOpen' src={Close} alt="" />
-                ) : (
-                    <img className='closeOpen' src={Megaphone} alt="" />
-                )
-                }
-            </button>
-            {buttonClicked &&
-                < Links />
-            }
-        </div>
-    )
-}
+  return (
+    <div className="button-container">
+      <button
+        className="button"
+        onClick={() => setButtonClicked(!buttonClicked)}
+      >
+        <AnimatePresence mode="wait">
+          {buttonClicked ? (
+            <motion.img
+              key="close"
+              className="closeOpen"
+              src={Close}
+              alt="Close Icon"
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              transition={{ duration: 0.3 }}
+            />
+          ) : (
+            <motion.img
+              key="megaphone"
+              className="closeOpen"
+              src={Megaphone}
+              alt="Megaphone Icon"
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              transition={{ duration: 0.3 }}
+            />
+          )}
+        </AnimatePresence>
+      </button>
+      {buttonClicked && <Links />}
+    </div>
+  );
+};
 
 export default Button;
